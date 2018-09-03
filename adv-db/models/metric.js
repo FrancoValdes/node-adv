98e490750963891b15f490e3f089f844170ae4f4
FrancoValdes <franco.valdes.p@gmail.com>
2018-09-03 11:00:46 -0300

    addModels

---
+++ adv-db/models/metric.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = async function setupMetricModel (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('metric', {
+    type: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    status: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    }
+  })
+}
