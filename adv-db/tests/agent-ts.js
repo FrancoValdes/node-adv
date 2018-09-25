37728a6a6895291a82496ea5bf041c30f4372e72
FrancoValdes <franco.valdes.p@gmail.com>
2018-09-25 13:03:50 -0300

    AgentTest&TapNyan

--- adv-db/tests/agent-ts.js
+++ adv-db/tests/agent-ts.js
@@ -1,29 +1,41 @@
 'use strict'
 
 const test = require('ava')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 
 let db = null
-
-let config = {
-  logging: () => {}
-}
-
-let MetricStub = {
-    belongsTo: () => {}
-}
+let MetricStub = null
+let AgentStub = null
+let sandbox = null
 
 test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
+
   AgentStub = {
-      hasMany: () => {}
+    hasMany: sandbox.spy()
   }
 
-  const setupDatabase = proxyquire('../',{
-      './models/agent': () => AgentStub,
-      './models/metric': () => MetricStub,
+  MetricStub = {
+    belongsTo: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
   })
   db = await setupDatabase(db)
 })
 
 test('Agent', t => {
   t.truthy(db.agent, 'Agent service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+})
+
+test.afterEach(() => {
+  sandbox && sandbox.restore()
 })
